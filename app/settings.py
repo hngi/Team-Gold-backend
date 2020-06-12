391494560d4d3bb254a23e2180b56bbbb2bd1908
Akorede Fodilu Olawale <afordeal88@gmail.com>
2020-06-12 18:30:21 +0100

    commit changes

--- app/settings.py
+++ app/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": "subscriber",
         "USER": "postgres",
-        "PASSWORD": "password",
+        "PASSWORD": "Olawale@1",
         "HOST": "localhost",
         "PORT": "5432",
     }
